584f763662162c98dea26354113eb37ab6fc57a7
xcv58 <chenyihonglove@gmail.com>
2016-04-07 13:54:43 -0400

    use Lock from package instead of Meteor.lock

--- examples/sample-app/client/main.js
+++ examples/sample-app/client/main.js
@@ -1,11 +1,12 @@
 import { Template } from 'meteor/templating';
+import { Lock } from 'meteor/xcv58:auth0-lock';
 import { Meteor } from 'meteor/meteor';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.hello.events({
   'click button.login': function () {
-    Meteor.lock.show();
+    Lock.show();
   },
   'click button.logout': function () {
     Meteor.logout();
