657d0cc3407ba5ec8fe88324723ee77499f02134
xcv58 <i@xcv58.com>
2018-02-03 16:55:35 -0800

    Fix test

--- auth0-lock/tests/client.js
+++ auth0-lock/tests/client.js
@@ -1,9 +1,9 @@
 import { Tinytest } from 'meteor/tinytest';
 
-import { Lock } from 'meteor/xcv58:auth0-lock';
+import { Lock, initLock } from 'meteor/xcv58:auth0-lock';
 
 // TODO: Unit test try Sinon.JS
 Tinytest.add('xcv58:auth0-lock - client', (test) => {
-  test.isFalse(Boolean(Lock), 'Lock is null or undefined');
-  test.isUndefined(Lock, 'Lock is null');
+  test.isTrue(Boolean(Lock), 'Lock is null or undefined');
+  test.isTrue(typeof initLock === 'function', 'initLock is a function');
 });
