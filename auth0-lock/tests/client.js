57503781b02c128e487daa782e958ab9185181ee
xcv58 <i@xcv58.com>
2018-02-03 16:58:22 -0800

    better syntax for test

--- auth0-lock/tests/client.js
+++ auth0-lock/tests/client.js
@@ -5,5 +5,5 @@ import { Lock, initLock } from 'meteor/xcv58:auth0-lock';
 // TODO: Unit test try Sinon.JS
 Tinytest.add('xcv58:auth0-lock - client', (test) => {
   test.isTrue(Boolean(Lock), 'Lock is null or undefined');
-  test.isTrue(typeof initLock === 'function', 'initLock is a function');
+  test.equal(typeof initLock, 'function', 'initLock is a function');
 });
